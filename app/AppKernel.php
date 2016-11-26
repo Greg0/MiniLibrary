2407eccc5b89a6252d33f75ddccff9c6a1cf6a46
Grego <gerg0sz92@gmail.com>
2016-11-26 15:51:09 +0100

    [core] Add sonata admin panel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,15 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            /* Test bundle */
             new AppBundle\AppBundle(),
+
+            /* Sonata Admin Bundle */
+            new \Sonata\CoreBundle\SonataCoreBundle(),
+            new \Sonata\BlockBundle\SonataBlockBundle(),
+            new \Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new \Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new \Sonata\AdminBundle\SonataAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
