47f00eddb3dbffe108014e9149ea0244fbbb891a
Grego <gerg0sz92@gmail.com>
2016-11-27 00:39:50 +0100

    [core] Add bundle configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,15 +16,17 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             /* Test bundle */
-            new AppBundle\AppBundle(),
+//            new AppBundle\AppBundle(),
 
-            /* Sonata Admin Bundle */
-            new \Sonata\CoreBundle\SonataCoreBundle(),
-            new \Sonata\BlockBundle\SonataBlockBundle(),
-            new \Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new \Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-            new \Sonata\AdminBundle\SonataAdminBundle(),
+            /* Library Bundle */
             new Greg0\LibraryBundle\LibraryBundle(),
+
+            /* FOS User Bundle */
+            new \FOS\UserBundle\FOSUserBundle(),
+
+            /* EasyAdmin */
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
